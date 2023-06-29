1052242d5a0e5a3a33bf6113fea2d8d016075798
Daniel Choi <schoiak@connect.ust.hk>
2023-06-29 16:26:17 +0900

    feat: 발급받은 쿠폰 슬라이드 리스트 UI 추가
    발급받은 쿠폰 탭 리스트 UI 추가

--- android/app/src/main/java/com/hyunprise/android/ui/member/coupon/IssuedCouponContainerActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/member/coupon/IssuedCouponContainerActivity.kt
@@ -2,13 +2,13 @@ package com.hyunprise.android.ui.member.coupon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hyunprise.android.databinding.ActivityCouponContainerBinding
-import com.hyunprise.android.ui.member.coupon.adaptors.CouponTabAdaptor
-import com.hyunprise.android.ui.member.coupon.fragments.CouponScrollingFragment
+import com.hyunprise.android.global.CouponConsts
+import com.hyunprise.android.ui.member.coupon.adaptors.IssuedCouponTabAdaptor
+import com.hyunprise.android.ui.member.coupon.fragments.IssuedCouponScrollingFragment
 
-class CouponContainerActivity : AppCompatActivity() {
+class IssuedCouponContainerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCouponContainerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +17,10 @@ class CouponContainerActivity : AppCompatActivity() {
         binding = ActivityCouponContainerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adaptor = CouponTabAdaptor(this)
-        val dataSet = mutableListOf("coupon1", "coupon2", "coupon3", "coupon4", "coupon5")
-        Log.d("sychoi", "adding CouponScrollingFragment")
-        adaptor.addFragment(CouponScrollingFragment(dataSet), "MY 쿠폰")
-        adaptor.addFragment(CouponScrollingFragment(dataSet), "사용한 쿠폰")
+        val adaptor = IssuedCouponTabAdaptor(this)
+        val memberUUID = "FF1342115E49E60FE05304001CACF958"
+        adaptor.addFragment(IssuedCouponScrollingFragment(memberUUID, CouponConsts.COUPON_STATUS_AVAILABLE), "MY 쿠폰")
+        adaptor.addFragment(IssuedCouponScrollingFragment(memberUUID, CouponConsts.COUPON_STATUS_USED), "사용한 쿠폰")
         binding.couponViewPager.adapter = adaptor
 
 
