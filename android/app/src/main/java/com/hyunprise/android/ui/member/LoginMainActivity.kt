b75b2e1eeedff5d01ddff1fcac878374d8e013da
Daniel Choi <schoiak@connect.ust.hk>
2023-06-29 11:01:57 +0900

    Fix: 설정오류 수정
    merge과정에서 생긴 Manifest 중복옵션 제거 및 import 에러 수정

--- android/app/src/main/java/com/hyunprise/android/ui/member/LoginMainActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/member/LoginMainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.hyunprise.android.MainActivity
 import com.hyunprise.android.databinding.ActivityLoginMainBinding
 import com.kakao.sdk.user.UserApiClient
 
