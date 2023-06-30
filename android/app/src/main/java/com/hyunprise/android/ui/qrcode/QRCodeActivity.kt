e89d4fbb189182480f04818c2814088804548eea
장서윤 <seoy316@naver.com>
2023-06-30 11:59:42 +0900

    fix: retrofit 적용

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/QRCodeActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/QRCodeActivity.kt
@@ -1,6 +1,5 @@
 package com.hyunprise.android.ui.qrcode
 
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +8,9 @@ import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.zxing.ResultPoint
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
+import com.hyunprise.android.api.coupon.services.IssuedCouponService
+import com.hyunprise.android.api.coupon.vo.Coupon
+import com.hyunprise.android.api.coupon.vo.IssuedCoupon
 import com.hyunprise.android.databinding.ActivityQrcodeBinding
 import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.BarcodeResult
@@ -19,17 +19,7 @@ import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.POST
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.lang.StringBuilder
-import java.net.HttpURLConnection
-import java.net.URL
 
 
 class QRCodeActivity: AppCompatActivity() {
@@ -37,6 +27,7 @@ class QRCodeActivity: AppCompatActivity() {
     lateinit var binding: ActivityQrcodeBinding
     lateinit var barcodeView: DecoratedBarcodeView
     lateinit var capture: CaptureManager
+    private val issuedCouponService = IssuedCouponService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +39,6 @@ class QRCodeActivity: AppCompatActivity() {
             android.Manifest.permission.CAMERA)
 
         if (camPermissionChecker == PackageManager.PERMISSION_GRANTED) {
-
             QRCodeReader(savedInstanceState)
         } else {
             ActivityCompat.requestPermissions(
@@ -107,44 +97,27 @@ class QRCodeActivity: AppCompatActivity() {
 
         barcodeView.decodeSingle(object : BarcodeCallback {
             override fun barcodeResult(result: BarcodeResult?) {
-                getIssuedCouponUUID(result)
+                val couponUUID = result.toString()
+                val memberUUID = "FF1342115E49E60FE05304001CACF958"
 
+                if (couponUUID != null) {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val issuedCoupon = IssuedCoupon(couponUUID = couponUUID, memberUUID = memberUUID)
+
+                        val couponData: Response<Coupon> = issuedCouponService.postIssuedCoupon(issuedCoupon)
+                        var intent = Intent(this@QRCodeActivity, CouponFoundActivity::class.java)
+                        Log.d("qrcode.issuedCoupon", "${couponData.body()?.couponName} ${couponData.body()?.couponDescription}")
+                        intent.putExtra("coupon_name", couponData.body()?.couponName)
+                        intent.putExtra("coupon_description", couponData.body()?.couponDescription)
+                        startActivity(intent)
+                    }
+                }
                 var intent = Intent(this@QRCodeActivity, CouponFoundActivity::class.java)
                 startActivity(intent)
                 finish()
             }
-            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) {
-            }
+
+            override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) { }
         })
-    }
-
-    fun getIssuedCouponUUID(result: BarcodeResult?) {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                var urlText=result?.text
-                val url= URL(urlText)
-                val urlConnection =url.openConnection() as HttpURLConnection
-                urlConnection.requestMethod = "GET"
-                if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
-                    val streamReader=InputStreamReader(urlConnection.inputStream)
-                    val buffered=BufferedReader(streamReader)
-
-                    val content=StringBuilder()
-                    while(true) {
-                        val line=buffered.readLine()?:break
-                        if ("<p>" in line)
-                            content.append(line)
-                    }
-                    buffered.close()
-                    urlConnection.disconnect()
-                    launch(Dispatchers.Main) {
-                        // 쿠폰 번호로 api 요청
-                        Log.d("log.qrcode", "${content.toString().replace("[^0-9]".toRegex(),"")}")
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
     }
 }
