ab86f5d358f68b75a70d17ecec94ce9280ef83cb
장서윤 <seoy316@naver.com>
2023-06-30 11:58:27 +0900

    fix: 생성자 추가
    IssuedCoupon 생성자 추가

--- android/app/src/main/java/com/hyunprise/android/api/coupon/vo/IssuedCoupon.kt
+++ android/app/src/main/java/com/hyunprise/android/api/coupon/vo/IssuedCoupon.kt
@@ -1,4 +1,22 @@
 package com.hyunprise.android.api.coupon.vo
 
-class IssuedCoupon {
+import java.sql.Date
+
+
+data class IssuedCoupon(
+    var issuedCouponUUID: String,
+    var memberUUID: String,
+    var couponUUID: String,
+    val couponCode: String = "defaultCode",
+    val status: Int = 0,
+    val issueDate: Date? = null,
+    val expirationDate: Date?= null
+) {
+    constructor(memberUUID: String, couponUUID: String)
+            : this(
+        issuedCouponUUID="defaultCode",
+        memberUUID = memberUUID,
+        couponUUID = couponUUID
+    )
 }
+
