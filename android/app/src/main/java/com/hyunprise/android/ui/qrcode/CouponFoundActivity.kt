568098147877803416a907d95d8b8ecdeaedc7dc
장서윤 <seoy316@naver.com>
2023-07-01 16:14:24 +0900

    feat: 쿠폰 받기 이벤트 구현
    쿠폰 발급 시 쿠폰 발급 페이지로 이동

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
@@ -4,9 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.hyunprise.android.api.coupon.services.IssuedCouponService
+import com.hyunprise.android.api.coupon.vo.IssuedCoupon
 import com.hyunprise.android.databinding.ActivityCouponFoundBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CouponFoundActivity : AppCompatActivity() {
+    private val issuedCouponService = IssuedCouponService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,7 +32,18 @@ class CouponFoundActivity : AppCompatActivity() {
         binding.couponFoundCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
 
         binding.couponFoundReceiveCouponBtn.setOnClickListener {
-            // 쿠폰 받기 이벤트 연결
+            CoroutineScope(Dispatchers.Main).launch {
+                val issuedCoupon = IssuedCoupon(
+                    couponUUID = intent.getStringExtra("coupon_uuid").toString(),
+                    memberUUID = intent.getStringExtra("member_uuid").toString()
+                )
+
+
+                val result = issuedCouponService.postIssuedCoupon(issuedCoupon)
+                Log.d("coupon_found_success", "$result")
+                Toast.makeText(this@CouponFoundActivity,
+                    "쿠폰이 발급 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.couponFoundReceivePointsBtn.setOnClickListener {
