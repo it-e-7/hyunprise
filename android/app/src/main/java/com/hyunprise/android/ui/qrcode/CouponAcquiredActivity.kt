afead2ea185906f5c841344fc5c1e82645245442
장서윤 <seoy316@naver.com>
2023-07-01 15:57:26 +0900

    feat: 쿠폰 받은 후 페이지 구현
    쿠폰 발견 페이지에서 쿠폰 받기 버튼을 눌렀을 때 나오는 화면

---
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponAcquiredActivity.kt
@@ -0,0 +1,32 @@
+package com.hyunprise.android.ui.qrcode
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyunprise.android.R
+import com.hyunprise.android.databinding.ActivityCouponAcquiredBinding
+
+class CouponAcquiredActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityCouponAcquiredBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.couponAcquiredCouponNameTv.text = intent.getStringExtra("coupon_name")
+        binding.couponAcquiredCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
+
+
+        binding.couponAcquiredContinueSearching.setOnClickListener {
+            finish()
+            var intent = Intent(this@CouponAcquiredActivity, QRCodeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.couponAcquiredCouponList.setOnClickListener {
+            // 쿠폰리스트 연결
+        }
+
+
+    }
+}
