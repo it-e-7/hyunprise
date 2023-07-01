412bdd95bcb8ec02162ff3e6cff84e439c99f047
tigre911 <tmvhswlqkqn_@naver.com>
2023-07-02 02:09:30 +0900

    feat: 메인화면 UI 레이아웃
    issue: #9

--- android/app/src/main/java/com/hyunprise/android/HomeActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/HomeActivity.kt
@@ -21,7 +21,6 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
--- android/app/src/main/java/com/hyunprise/android/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/home/HomeFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.hyunprise.android.R
 import com.hyunprise.android.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -26,4 +30,25 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 버튼 클릭 이벤트 처리
+        binding.menuBarButton.setOnClickListener {
+            // DrawerLayout 표시
+            showDrawerLayout()
+        }
+    }
+
+    private fun showDrawerLayout() {
+        // DrawerLayout을 열기 위해 Activity에 접근
+        val activity = requireActivity() as AppCompatActivity
+        val drawerLayout = activity.findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        // 오른쪽에 위치한 네비게이션 드로어를 가져와서 열기
+        val rightDrawer = activity.findViewById<LinearLayout>(R.id.right_drawer)
+        drawerLayout.openDrawer(rightDrawer)
+    }
+
 }
