3ae4c3dbfe16a14dd30e2b5bc5dc2ff46e2e6ceb
장서윤 <seoy316@naver.com>
2023-07-01 18:38:01 +0900

    feat: 홈으로 돌아가기 구현
    홈 버튼 누르면 홈액티비티로 이동

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponAcquiredActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponAcquiredActivity.kt
@@ -3,8 +3,7 @@ package com.hyunprise.android.ui.qrcode
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.hyunprise.android.R
+import com.hyunprise.android.HomeActivity
 import com.hyunprise.android.databinding.ActivityCouponAcquiredBinding
 
 class CouponAcquiredActivity : AppCompatActivity() {
@@ -16,6 +15,13 @@ class CouponAcquiredActivity : AppCompatActivity() {
 
         binding.couponAcquiredCouponNameTv.text = intent.getStringExtra("coupon_name")
         binding.couponAcquiredCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
+
+
+        binding.couponAcquiredExit.setOnClickListener {
+            finish()
+            val intent = Intent(this@CouponAcquiredActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
         binding.couponAcquiredContinueSearching.setOnClickListener {
             finish()
