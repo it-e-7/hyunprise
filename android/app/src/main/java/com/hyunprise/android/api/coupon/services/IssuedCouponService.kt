38403085f3bb2b04bc8950f03a32dc04e568445c
Daniel Choi <schoiak@connect.ust.hk>
2023-07-02 20:12:08 +0900

    feat: API Handler 추가
    Retrofit API 응답오류를 처리해주는 Object Class 구현

--- android/app/src/main/java/com/hyunprise/android/api/coupon/services/IssuedCouponService.kt
+++ android/app/src/main/java/com/hyunprise/android/api/coupon/services/IssuedCouponService.kt
@@ -1,30 +1,28 @@
 package com.hyunprise.android.api.coupon.services
 
-import android.util.Log
 import com.hyunprise.android.api.RetrofitConfig
 import com.hyunprise.android.api.coupon.clients.IssuedCouponClient
 import com.hyunprise.android.api.coupon.vo.Coupon
+import com.hyunprise.android.api.coupon.vo.CouponDetail
 import com.hyunprise.android.api.coupon.vo.CouponSummary
 import com.hyunprise.android.api.coupon.vo.IssuedCoupon
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.hyunprise.android.global.api.ApiHandler
 import retrofit2.Response
 
-class IssuedCouponService {
+object IssuedCouponService {
 
     private val retrofit = RetrofitConfig.retrofit_gson
     private val issuedCouponClient = retrofit.create(IssuedCouponClient::class.java)
-    suspend fun fetchData(memberUUID: String, available: Boolean): List<CouponSummary> {
-        return withContext(Dispatchers.Main) {
-            val response = issuedCouponClient.getAllCouponsOfMemberByStatus(memberUUID, available)
-            if (response.isSuccessful) {
-                response.body() ?: emptyList()
-            }
-            else {
-                Log.d("sychoi", "Error ${response.raw()}")
-                emptyList()
-            }
-        }
+    suspend fun getAllCouponsOfMemberByStatus(memberUUID: String, available: Boolean): List<CouponSummary> {
+        return ApiHandler.safeAPICall {
+            issuedCouponClient.getAllCouponsOfMemberByStatus(memberUUID, available)
+        } ?: emptyList()
+    }
+
+    suspend fun getIssuedCouponByIssuedCouponUUID(couponUUID: String): CouponDetail {
+        return ApiHandler.safeAPICall {
+            issuedCouponClient.getIssuedCouponByIssuedCouponUUID(couponUUID)
+        } ?: CouponDetail()
     }
     
     suspend fun postIssuedCoupon(issuedCoupon: IssuedCoupon) : Response<Coupon> {
