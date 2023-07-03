eb0cc73b75d266fdb790639c353ea3066f85afbb
장서윤 <seoy316@naver.com>
2023-07-03 09:45:11 +0900

    feat: 발급된 쿠폰 정보 조회 api 호출
    쿠폰 획득 페이지에 intent로 넘김

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.hyunprise.android.api.coupon.services.IssuedCouponService
+import com.hyunprise.android.api.coupon.vo.CouponDetail
 import com.hyunprise.android.api.coupon.vo.IssuedCoupon
 import com.hyunprise.android.databinding.ActivityCouponFoundBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CouponFoundActivity : AppCompatActivity() {
     private val issuedCouponService = IssuedCouponService()
@@ -27,11 +30,8 @@ class CouponFoundActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val coupon_name = intent.getStringExtra("coupon_name")
-        val coupon_description= intent.getStringExtra("coupon_description")
-
-        binding.couponFoundCouponNameTv.text = coupon_name
-        binding.couponFoundCouponDescriptionTv.text = coupon_description
+        binding.couponFoundCouponNameTv.text = intent.getStringExtra("coupon_name")
+        binding.couponFoundCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
 
         binding.couponFoundReceiveCouponBtn.setOnClickListener {
             CoroutineScope(Dispatchers.Main).launch {
@@ -45,8 +45,23 @@ class CouponFoundActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         Toast.makeText(this@CouponFoundActivity, "쿠폰이 발급 되었습니다.", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this@CouponFoundActivity, CouponAcquiredActivity::class.java)
-                        intent.putExtra("coupon_name", coupon_name)
-                        intent.putExtra("coupon_description", coupon_description)
+                        val issuedCoupon: Response<CouponDetail> = issuedCouponService.getIssuedCoupon(response.body().toString())
+
+                        val issuedCouponDetail = issuedCoupon.body()
+
+                        val expirationDate = issuedCouponDetail?.expirationDate
+                        val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+                        val formattedDate = formatter.format(expirationDate)
+
+                        intent.putExtra("coupon_name", issuedCouponDetail?.couponName)
+                        intent.putExtra("coupon_retail_location", issuedCouponDetail?.retailerLocation)
+                        intent.putExtra("coupon_expiration_date", formattedDate)
+                        intent.putExtra("coupon_coupon_code", issuedCouponDetail?.couponCode)
+                        intent.putExtra("coupon_description", issuedCouponDetail?.couponDescription)
+
+                        Log.d("log.expirationDate.day", "${formattedDate}")
+                        Log.d("log.issuedCoupon", "${issuedCoupon.body()}")
+
                         finish()
                         startActivity(intent)
                     } else {
