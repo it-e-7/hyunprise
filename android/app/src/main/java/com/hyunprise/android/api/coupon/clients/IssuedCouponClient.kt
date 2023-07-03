db6a370b981d95c7e2369a7cfa90ef05370e520c
장서윤 <seoy316@naver.com>
2023-07-03 09:33:38 +0900

    feat: 발급된 쿠폰 정보 조회 api 구현

--- android/app/src/main/java/com/hyunprise/android/api/coupon/clients/IssuedCouponClient.kt
+++ android/app/src/main/java/com/hyunprise/android/api/coupon/clients/IssuedCouponClient.kt
@@ -1,6 +1,7 @@
 package com.hyunprise.android.api.coupon.clients
 
 import com.hyunprise.android.api.coupon.vo.Coupon
+import com.hyunprise.android.api.coupon.vo.CouponDetail
 import com.hyunprise.android.api.coupon.vo.CouponSummary
 import com.hyunprise.android.api.coupon.vo.IssuedCoupon
 import retrofit2.Response
@@ -8,6 +9,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface IssuedCouponClient {
@@ -21,4 +23,11 @@ interface IssuedCouponClient {
     @Headers("Content-Type: application/json")
     @POST("issued_coupon")
     suspend fun postOneIssuedCoupon(@Body issuedCoupon: IssuedCoupon): Response<String>
+
+    @Headers("Content-Type: application/json")
+    @GET("issued_coupon/{issuedCouponUUID}")
+    suspend fun getOneIssuedCoupon (
+        @Path("issuedCouponUUID") issuedCouponUUID: String,
+    ): Response<CouponDetail>
+
 }
--- android/app/src/main/java/com/hyunprise/android/api/coupon/services/IssuedCouponService.kt
+++ android/app/src/main/java/com/hyunprise/android/api/coupon/services/IssuedCouponService.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.hyunprise.android.api.RetrofitConfig
 import com.hyunprise.android.api.coupon.clients.IssuedCouponClient
 import com.hyunprise.android.api.coupon.vo.Coupon
+import com.hyunprise.android.api.coupon.vo.CouponDetail
 import com.hyunprise.android.api.coupon.vo.CouponSummary
 import com.hyunprise.android.api.coupon.vo.IssuedCoupon
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +15,7 @@ class IssuedCouponService {
 
     private val retrofit = RetrofitConfig.retrofit_gson
     private val issuedCouponClient = retrofit.create(IssuedCouponClient::class.java)
+
 /*    suspend fun fetchData(memberUUID: String, status: Int): List<CouponSummary> {
         return withContext(Dispatchers.Main) {
             val response = issuedCouponClient.getAllCouponsOfMemberByStatus(memberUUID, available)
@@ -28,12 +30,26 @@ class IssuedCouponService {
     }*/
     
     suspend fun postIssuedCoupon(issuedCoupon: IssuedCoupon) : Response<String> {
+
         val response = issuedCouponClient.postOneIssuedCoupon(issuedCoupon)
         response.body() ?: emptyList<String>()
         try {
             Log.d("postIssuedCoupon", "${response.body()}")
         } catch (e: Exception) {
             Log.d("postIssuedCoupon.error", "${e.printStackTrace()}")
+        }
+
+        return response
+    }
+
+    suspend fun getIssuedCoupon(issuedCouponUUID: String) : Response<CouponDetail> {
+
+        val response = issuedCouponClient.getOneIssuedCoupon(issuedCouponUUID)
+        response.body() ?: emptyList<String>()
+        try {
+            Log.d("getIssuedCoupon", "${response.body()}")
+        } catch (e: Exception) {
+            Log.d("getIssuedCoupon.error", "${e.printStackTrace()}")
         }
 
         return response
