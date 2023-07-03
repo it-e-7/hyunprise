9ef0c05c551c89feb3aa39e7e9ec92422eb488d7
장서윤 <seoy316@naver.com>
2023-07-03 20:21:33 +0900

    chore: qr or barcode dependency 추가
    안드로이드

--- android/app/src/main/java/com/hyunprise/android/api/RetrofitConfig.kt
+++ android/app/src/main/java/com/hyunprise/android/api/RetrofitConfig.kt
@@ -1,13 +1,18 @@
 package com.hyunprise.android.api
 
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitConfig {
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
-    private val gson: GsonConverterFactory = GsonConverterFactory.create()
+    private const val BASE_URL = "https://2d9b-112-221-184-61.ngrok-free.app/api/"
+    private val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
     val retrofit_gson: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(gson)
+        .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 }
