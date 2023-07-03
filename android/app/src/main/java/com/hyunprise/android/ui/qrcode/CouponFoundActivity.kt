6277bcac8ec09525d578af53c6693c45e70e4fbc
장서윤 <seoy316@naver.com>
2023-07-03 20:13:36 +0900

    fix: 쿠폰 서비스 싱글톤 적용
    class에서 object로 변경된 service 호출

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
@@ -14,10 +14,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 class CouponFoundActivity : AppCompatActivity() {
-    private val issuedCouponService = IssuedCouponService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,48 +32,62 @@ class CouponFoundActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val cal = Calendar.getInstance()
+        val df = SimpleDateFormat("yyyy.MM.dd", Locale("ko", "KR"))
+        val currentDate = df.format(cal.time)
+        cal.add(Calendar.DATE, 1)
+        val lastDate = df.format(cal.time).toString()
+
+        val period = "$currentDate ~ $lastDate"
+
         binding.couponFoundCouponNameTv.text = intent.getStringExtra("coupon_name")
-        binding.couponFoundCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
+        binding.couponFoundRetailerLocation.text = intent.getStringExtra("retailer_location")
 
-        binding.couponFoundReceiveCouponBtn.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                val couponUUID = intent.getStringExtra("coupon_uuid").toString()
-                val memberUUID= intent.getStringExtra("member_uuid").toString()
-                val issuedCoupon = IssuedCoupon(
-                    couponUUID = couponUUID, memberUUID = memberUUID
-                )
-                try {
-                    val response: Response<String> = issuedCouponService.postIssuedCoupon(issuedCoupon)
-                    if (response.isSuccessful) {
-                        Toast.makeText(this@CouponFoundActivity, "쿠폰이 발급 되었습니다.", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this@CouponFoundActivity, CouponAcquiredActivity::class.java)
-                        val issuedCoupon: Response<CouponDetail> = issuedCouponService.getIssuedCoupon(response.body().toString())
+        intent.getStringExtra("retailer_location")
+            ?.let { Log.d("coupon_found_retailer_location", it) }
+        binding.couponFoundExpirationPeriodTv.text = period
 
-                        val issuedCouponDetail = issuedCoupon.body()
+//        binding.couponFoundCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
 
-                        val expirationDate = issuedCouponDetail?.expirationDate
-                        val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
-                        val formattedDate = formatter.format(expirationDate)
+            binding.couponFoundReceiveCouponBtn.setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val couponUUID = intent.getStringExtra("coupon_uuid").toString()
+                    val memberUUID= intent.getStringExtra("member_uuid").toString()
+                    val issuedCoupon = IssuedCoupon(
+                        couponUUID = couponUUID, memberUUID = memberUUID
+                    )
+                    try {
+                        val response: Response<String> = IssuedCouponService.postIssuedCoupon(issuedCoupon)
+                        if (response.isSuccessful) {
+                            Toast.makeText(this@CouponFoundActivity, "쿠폰이 발급 되었습니다.", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this@CouponFoundActivity, CouponAcquiredActivity::class.java)
+                            val issuedCoupon: Response<CouponDetail> = IssuedCouponService.getIssuedCoupon(response.body().toString())
 
-                        intent.putExtra("coupon_name", issuedCouponDetail?.couponName)
-                        intent.putExtra("coupon_retail_location", issuedCouponDetail?.retailerLocation)
-                        intent.putExtra("coupon_expiration_date", formattedDate)
-                        intent.putExtra("coupon_coupon_code", issuedCouponDetail?.couponCode)
-                        intent.putExtra("coupon_description", issuedCouponDetail?.couponDescription)
+                            val issuedCouponDetail = issuedCoupon.body()
 
-                        Log.d("log.expirationDate.day", "${formattedDate}")
-                        Log.d("log.issuedCoupon", "${issuedCoupon.body()}")
+                            val expirationDate = issuedCouponDetail?.expirationDate
+                            val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+                            val formattedDate = formatter.format(expirationDate)
 
-                        finish()
-                        startActivity(intent)
-                    } else {
-                        Log.e("log.coupon_found_fail", "${response.errorBody()}")
+                            intent.putExtra("coupon_name", issuedCouponDetail?.couponName)
+                            intent.putExtra("coupon_retail_location", issuedCouponDetail?.retailerLocation)
+                            intent.putExtra("coupon_expiration_date", formattedDate)
+                            intent.putExtra("coupon_coupon_code", issuedCouponDetail?.couponCode)
+                            intent.putExtra("coupon_description", issuedCouponDetail?.couponDescription)
+
+                            Log.d("log.expirationDate.day", "${formattedDate}")
+                            Log.d("log.issuedCoupon", "${issuedCoupon.body()}")
+
+                            finish()
+                            startActivity(intent)
+                        } else {
+                            Log.e("log.coupon_found_fail", "${response.errorBody()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.e("log.coupon_found_error", "${e.message}")
                     }
-                } catch (e: Exception) {
-                    Log.e("log.coupon_found_error", "${e.message}")
                 }
-            }
-        }
+            }.toString()
 
         binding.couponFoundReceivePointsBtn.setOnClickListener {
             // 포인트 받기 이벤트 연결
