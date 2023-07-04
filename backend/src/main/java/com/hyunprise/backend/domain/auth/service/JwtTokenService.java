d0a17b018e6a4d3698baacfea4dfc81da44fd0b5
Daniel Choi <schoiak@connect.ust.hk>
2023-07-04 21:10:31 +0900

    remove: 미사용 클래스 삭제

--- backend/src/main/java/com/hyunprise/backend/domain/auth/service/JwtTokenService.java
+++
@@ -1,8 +0,0 @@
-package com.hyunprise.backend.domain.auth.service;
-
-import com.hyunprise.backend.domain.auth.vo.JwtToken;
-
-public interface JwtTokenService {
-
-    void saveJwtToken(JwtToken jwtToken);
-}
