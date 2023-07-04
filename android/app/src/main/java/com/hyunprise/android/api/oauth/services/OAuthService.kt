dac7cd3bc4d92106a65ca369b04b3c8deb7d21c3
Daniel Choi <schoiak@connect.ust.hk>
2023-07-05 02:07:39 +0900

    feat: Android OAuth 서비스 추가
    기존 loginService 삭제

---
+++ android/app/src/main/java/com/hyunprise/android/api/oauth/services/OAuthService.kt
@@ -0,0 +1,25 @@
+package com.hyunprise.android.api.oauth.services
+
+import android.util.Log
+import com.hyunprise.android.api.RetrofitConfig
+import com.hyunprise.android.api.oauth.clients.OAuthClient
+import com.hyunprise.android.api.oauth.vo.OAuth
+import com.hyunprise.android.api.oauth.vo.OAuthProvider
+import com.hyunprise.android.api.oauth.vo.OAuthResult
+import com.hyunprise.android.global.api.ApiHandler
+
+object OAuthService {
+    private val retrofit = RetrofitConfig.retrofit_gson
+    private val oAuthClient = retrofit.create(OAuthClient::class.java)
+
+    suspend fun authroizeKakao(oAuth: OAuth): OAuthResult {
+        oAuth.provider = OAuthProvider.KAKAO
+        return authorize(oAuth)
+    }
+    private suspend fun authorize(oAuth: OAuth): OAuthResult {
+        Log.d("login.log", "oAuth obj $oAuth")
+        return ApiHandler.safeAPICall {
+            oAuthClient.authorize(oAuth)
+        } ?: OAuthResult()
+    }
+}
