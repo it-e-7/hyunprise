16fbbf0a270465cac6c5e43d628a0dec7d8e2e3c
Daniel Choi <schoiak@connect.ust.hk>
2023-07-05 02:08:19 +0900

    feat: Retrofit 헤더 추가
    로그인 시 토큰값을 헤더에 추가하도록 변경

--- android/app/src/main/java/com/hyunprise/android/api/RetrofitConfig.kt
+++ android/app/src/main/java/com/hyunprise/android/api/RetrofitConfig.kt
@@ -1,20 +1,42 @@
 package com.hyunprise.android.api
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.hyunprise.android.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitConfig {
     private const val BASE_URL = BuildConfig.BACKEND_BASE_URL
-
-    private val gson: Gson = GsonBuilder()
+    private val gsonFactory: GsonConverterFactory = GsonConverterFactory.create(GsonBuilder()
         .setLenient()
-        .create()
-    val retrofit_gson: Retrofit = Retrofit.Builder()
+        .create())
+    var retrofit_gson: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addConverterFactory(gsonFactory)
         .build()
 
+    private class HeaderInterceptor(private val token: String): Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request()
+            val newRequest = request.newBuilder()
+                .header("Authorization", "Bearer $token")
+                .build()
+            return chain.proceed(newRequest)
+        }
+    }
+
+    fun patchAuthorizationHeader(token: String) {
+        retrofit_gson = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HeaderInterceptor(token)) // Add the HeaderInterceptor
+                    .build()
+            )
+            .addConverterFactory(gsonFactory)
+            .build()
+    }
 }
