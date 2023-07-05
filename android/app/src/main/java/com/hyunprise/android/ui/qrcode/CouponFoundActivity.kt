0bf5461ccd8702534a55b14402058d8f6379969c
장서윤 <seoy316@naver.com>
2023-07-06 02:06:16 +0900

    feat: 포인트 받기 연결

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponFoundActivity.kt
@@ -37,8 +37,10 @@ class CouponFoundActivity : AppCompatActivity() {
 
         val period = "$currentDate ~ $lastDate"
 
+        binding.couponFoundBrandNameTv.text = intent.getStringExtra("brand_name")
         binding.couponFoundCouponNameTv.text = intent.getStringExtra("coupon_name")
         binding.couponFoundRetailerLocation.text = intent.getStringExtra("retailer_location")
+        binding.couponFoundReceivePointsBtn.text = "${intent.getIntExtra("equivalent_point", -1).toString()} 포인트 받기"
 
         intent.getStringExtra("retailer_location")
             ?.let { Log.d("coupon_found_retailer_location", it) }
@@ -46,13 +48,14 @@ class CouponFoundActivity : AppCompatActivity() {
 
 //        binding.couponFoundCouponDescriptionTv.text = intent.getStringExtra("coupon_description")
 
+        val couponUUID = intent.getStringExtra("coupon_uuid").toString()
+        val memberUUID= intent.getStringExtra("member_uuid").toString()
+        val issuedCoupon = IssuedCoupon(
+            couponUUID = couponUUID, memberUUID = memberUUID
+        )
+
         binding.couponFoundReceiveCouponBtn.setOnClickListener {
                 CoroutineScope(Dispatchers.Main).launch {
-                    val couponUUID = intent.getStringExtra("coupon_uuid").toString()
-                    val memberUUID= intent.getStringExtra("member_uuid").toString()
-                    val issuedCoupon = IssuedCoupon(
-                        couponUUID = couponUUID, memberUUID = memberUUID
-                    )
                     try {
                         val response = issuedCouponService.postIssuedCoupon(issuedCoupon)
                         Log.d("log.response", "${response}")
@@ -73,6 +76,8 @@ class CouponFoundActivity : AppCompatActivity() {
                             intent.putExtra("coupon_expiration_date", formattedDate)
                             intent.putExtra("coupon_coupon_code", issuedCouponDetail?.couponCode)
                             intent.putExtra("coupon_description", issuedCouponDetail?.couponDescription)
+                            intent.putExtra("brand_name", issuedCouponDetail?.brandName)
+                            Log.d("issuedCouponDetail?.brandName", "${issuedCouponDetail?.brandName}")
                             finish()
                             startActivity(intent)
                         } else {
@@ -86,6 +91,39 @@ class CouponFoundActivity : AppCompatActivity() {
 
         binding.couponFoundReceivePointsBtn.setOnClickListener {
             // 포인트 받기 이벤트 연결
-        }
+            CoroutineScope(Dispatchers.Main).launch {
+                try {
+                    val response = issuedCouponService.postIssuedCoupon(issuedCoupon)
+                    Log.d("log.response", "${response}")
+
+                    if (response.isNotEmpty()) {
+                        Toast.makeText(this@CouponFoundActivity, "포인트가 지급 되었습니다.", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@CouponFoundActivity, PointAcquiredActivity::class.java)
+                        val issuedCouponDetail = issuedCouponService.getIssuedCoupon(response)
+
+                        Log.d("log.issuedCouponDetail", "${issuedCouponDetail}")
+
+                        val expirationDate = issuedCouponDetail?.expirationDate
+                        val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+                        val formattedDate = formatter.format(expirationDate)
+
+                        intent.putExtra("coupon_name", issuedCouponDetail?.couponName)
+                        intent.putExtra("coupon_retail_location", issuedCouponDetail?.retailerLocation)
+                        intent.putExtra("coupon_expiration_date", formattedDate)
+                        intent.putExtra("coupon_coupon_code", issuedCouponDetail?.couponCode)
+                        intent.putExtra("coupon_description", issuedCouponDetail?.couponDescription)
+                        intent.putExtra("equivalent_point", issuedCouponDetail?.equivalentPoint)
+                        intent.putExtra("brand_name", issuedCouponDetail?.brandName)
+
+                        finish()
+                        startActivity(intent)
+                    } else {
+                        Log.e("log.coupon_found_fail", "${response}")
+                    }
+                } catch (e: Exception) {
+                    Log.e("log.coupon_found_error", "${e.message}")
+                }
+            }
+        }.toString()
     }
 }
