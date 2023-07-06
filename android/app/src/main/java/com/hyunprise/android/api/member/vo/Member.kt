1683f63d210593432997048ca06c789362d80b89
Daniel Choi <schoiak@connect.ust.hk>
2023-07-06 11:54:59 +0900

    feat: 로그인 시 유저 정보 받아오는 기능 추가

---
+++ android/app/src/main/java/com/hyunprise/android/api/member/vo/Member.kt
@@ -0,0 +1,17 @@
+package com.hyunprise.android.api.member.vo
+
+import java.sql.Timestamp
+
+data class Member (
+    val memberUUID: String? = null,
+    val authenticationId: Long? = null,
+    val memberEmail: String? = null,
+    val memberName: String? = null,
+    val membershipPoint: Int? = null,
+    val gender: Int? = null,
+    val birthdate: Timestamp? = null,
+    val address: String? = null,
+    val contact: String? = null,
+    val accountType: Int? = null,
+    val brandId: Long? = null
+)
--- android/app/src/main/java/com/hyunprise/android/global/api/ApiHandler.kt
+++ android/app/src/main/java/com/hyunprise/android/global/api/ApiHandler.kt
@@ -7,16 +7,18 @@ import retrofit2.Response
 
 object ApiHandler {
     suspend inline fun <T> safeAPICall(crossinline apiCall: suspend () -> Response<T>): T? {
-        return withContext(Dispatchers.Main) {
+        return withContext(Dispatchers.IO) {
             try {
                 val response = apiCall()
                 if (response.isSuccessful) {
+                    Log.d("sychoi", "Success. ${response.body()}")
                     response.body()
                 } else {
+                    Log.d("sychoi", "Got response but failed. $response")
                     null
                 }
             } catch (e: Exception) {
-                Log.d("sychoi", e.message.toString())
+                Log.d("sychoi", "API call Error ${e.message}")
                 null
             }
         }
