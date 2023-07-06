fed5586941f16342d34fccdf0cf4031d6b27e313
tigre911 <tmvhswlqkqn_@naver.com>
2023-07-06 11:48:19 +0900

    feat: poitn shop button 추가
    issue: #9

--- android/app/src/main/java/com/hyunprise/android/ui/pointshop/PointShopFragment.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/pointshop/PointShopFragment.kt
@@ -1,10 +1,12 @@
 package com.hyunprise.android.ui.pointshop
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hyunprise.android.HomeActivity
 import com.hyunprise.android.databinding.FragmentPointShopBinding
 
 
@@ -19,6 +21,11 @@ class PointShopFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPointShopBinding.inflate(inflater, container, false)
+
+        binding.pointShopBackBtn.setOnClickListener{
+            val intent = Intent(activity, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
         return binding.root
     }
