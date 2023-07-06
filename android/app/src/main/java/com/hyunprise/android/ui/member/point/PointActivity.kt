fe9198fdc4f847d389dc56ce920c693e53b8a3d8
tigre911 <tmvhswlqkqn_@naver.com>
2023-07-06 11:47:34 +0900

    feat: 포인트 페이지 비디오(광고)기능추가
    issue: #9

--- android/app/src/main/java/com/hyunprise/android/ui/member/point/PointActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/member/point/PointActivity.kt
@@ -1,7 +1,10 @@
 package com.hyunprise.android.ui.member.point
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 import com.hyunprise.android.CodeGenerate
 import com.hyunprise.android.R
 import com.hyunprise.android.databinding.ActivityPointBinding
@@ -12,14 +15,20 @@ class PointActivity : AppCompatActivity() {
         var binding = ActivityPointBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var pointBarcode =  CodeGenerate()
+        var pointBarcode = CodeGenerate()
 
         var pointBarcodeInfo = pointBarcode.generateBitmapBarCode("1234123412341234")
 
         binding.pointBarcodeImg.setImageBitmap(pointBarcodeInfo)
 
         binding.pointBackButton.setOnClickListener {
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
         }
+
+        val videoView = binding.myPointVideo
+        videoView.setMediaController(MediaController(this))
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.my_point_video)
+        videoView.setVideoURI(videoUri)
+        videoView.start()
     }
 }
