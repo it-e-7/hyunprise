88f3f6466d52410fe286b124147d37ee5abba717
tigre911 <tmvhswlqkqn_@naver.com>
2023-07-07 10:13:18 +0900

    feat: 폭죽 애니메이션 추가
    issue: #100

--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponAcquiredActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/CouponAcquiredActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.hyunprise.android.CodeGenerate
 import com.hyunprise.android.HomeActivity
+import com.hyunprise.android.R
 import com.hyunprise.android.databinding.ActivityCouponAcquiredBinding
+import com.hyunprise.android.ui.admin.coupon.Presets
+import com.hyunprise.android.ui.admin.coupon.Presets.Companion.explode
+import com.hyunprise.android.ui.admin.coupon.Presets.Companion.festive
+import com.hyunprise.android.ui.admin.coupon.Presets.Companion.parade
 import com.hyunprise.android.ui.member.coupon.IssuedCouponContainerActivity
+import nl.dionsegijn.konfetti.xml.KonfettiView
 
 class CouponAcquiredActivity : AppCompatActivity() {
+
+    private lateinit var viewKonfetti: KonfettiView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,5 +52,35 @@ class CouponAcquiredActivity : AppCompatActivity() {
             val intent = Intent(this@CouponAcquiredActivity, IssuedCouponContainerActivity::class.java)
             startActivity(intent)
         }
+
+        viewKonfetti = binding.konfettiView
+        festive()
+        explode()
+        parade()
+        rain()
+    }
+    private fun festive() {
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.festive())
+    }
+    private fun explode() {
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.explode())
+    }
+    private fun parade() {
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.parade())
+    }
+    private fun rain() {
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.rain())
     }
 }
--- android/app/src/main/java/com/hyunprise/android/ui/qrcode/PointAcquiredActivity.kt
+++ android/app/src/main/java/com/hyunprise/android/ui/qrcode/PointAcquiredActivity.kt
@@ -1,23 +1,34 @@
 package com.hyunprise.android.ui.qrcode
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.hyunprise.android.CodeGenerate
 import com.hyunprise.android.HomeActivity
+import com.hyunprise.android.R
 import com.hyunprise.android.databinding.ActivityCouponAcquiredBinding
 import com.hyunprise.android.databinding.ActivityPointAcquiredBinding
+import com.hyunprise.android.ui.admin.coupon.Presets
 import com.hyunprise.android.ui.member.point.PointActivity
+import nl.dionsegijn.konfetti.core.models.Shape
+import nl.dionsegijn.konfetti.core.models.Size
+import nl.dionsegijn.konfetti.xml.KonfettiView
 
 class PointAcquiredActivity : AppCompatActivity() {
+    private lateinit var viewKonfetti: KonfettiView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityPointAcquiredBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pointAqcuiredPointTv.text = "${intent.getIntExtra("equivalent_point", -1).toString()} 포인트"
+        binding.pointAqcuiredPointTv.text =
+            "${intent.getIntExtra("equivalent_point", -1).toString()} 포인트"
 
         binding.pointAqcuiredExit.setOnClickListener {
             finish()
@@ -37,5 +48,38 @@ class PointAcquiredActivity : AppCompatActivity() {
             val intent = Intent(this@PointAcquiredActivity, PointActivity::class.java)
             startActivity(intent)
         }
+
+        val scaleUpAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.scale_animation)
+        binding.pointAqcuiredPointTv.startAnimation(scaleUpAnimation)
+
+        viewKonfetti = binding.konfettiView
+        festive()
+        explode()
+        parade()
+//        rain()
     }
+    private fun festive() {
+            /**
+             * See [Presets] for this configuration
+             */
+            viewKonfetti.start(Presets.festive())
+        }
+    private fun explode() {
+            /**
+             * See [Presets] for this configuration
+             */
+            viewKonfetti.start(Presets.explode())
+        }
+    private fun parade() {
+            /**
+             * See [Presets] for this configuration
+             */
+            viewKonfetti.start(Presets.parade())
+        }
+    private fun rain() {
+            /**
+             * See [Presets] for this configuration
+             */
+            viewKonfetti.start(Presets.rain())
+        }
 }
